38e2fc9b196e7f772133b7b55ec21bb8f66eb0b0
gsmile0102 <gsmile0102@gmail.com>
2018-01-11 17:06:51 +0800

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCLMCWz9zHKmSsT48meG_7w5x7U2tUbGqw",
+  authDomain: "af2-lists-4fd96.firebaseapp.com",
+  databaseURL: "https://af2-lists-4fd96.firebaseio.com",
+  projectId: "af2-lists-4fd96",
+  storageBucket: "af2-lists-4fd96.appspot.com",
+  messagingSenderId: "1060563833216"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +26,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
